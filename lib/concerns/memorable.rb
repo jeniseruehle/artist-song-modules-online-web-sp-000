edab90ef919bef85b570885a28bbd96ab15ddc6c
Jenise Ruehle <jela07@gmail.com>
2020-03-28 16:30:51 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,11 +1,11 @@
 module Memorable
   module ClassMethods
     def reset_all
-    self.all.clear
+      self.all.clear
     end
 
     def count
-    self.all.count
+      self.all.count
     end
   end
 end 
